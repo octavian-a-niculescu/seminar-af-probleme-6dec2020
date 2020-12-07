03d8f56b78c32c6b70f2db01a384ecb2636b4f0d
octavian-a-niculescu <33459754+octavian-a-niculescu@users.noreply.github.com>
2020-12-07 19:04:13 +0200

    Update interfata-header.h

--- interfata-header.h
+++ interfata-header.h
@@ -6,16 +6,7 @@ struct Obiect {
 
 	void Citire();
 	double Raport();
-	bool operator < (Obiect obiect2)
-	{
-		return _greutate / _beneficiu < obiect2._greutate / obiect2._beneficiu;
-	}
-	bool operator > (Obiect obiect2)
-	{
-		return _greutate / _beneficiu > obiect2._greutate / obiect2._beneficiu;
-	}
-	bool operator == (Obiect obiect2)
-	{
-		return _greutate / _beneficiu == obiect2._greutate / obiect2._beneficiu;
-	}
+	bool operator < (Obiect obiect2);
+	bool operator > (Obiect obiect2);
+	bool operator == (Obiect obiect2);
 };
